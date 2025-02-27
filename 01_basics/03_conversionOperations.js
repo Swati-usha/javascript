467f80ecd4092a6d3b713cf642753cdc2bcfb4cc
Swati Tyagi <57513959+Swati-usha@users.noreply.github.com>
2025-02-27 07:00:15 +0000

    datatype conversions

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+let score = undefined
+
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);//String
+// console.log(valueInNumber);// NaN
+
+//"33"=> 33
+// "33abc"=> NaN
+// true=> 1; flase=>0
+
+
+// for null, it takes 0 as a value.
+// for undefined it will show NaN
+// for boolean value it will show 0 for false and 1 for true.
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+//"" => false;
+// 1=> true, 0 => false, "swati =>true"
+
+let someNumber = 34;
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof (stringNumber))
+//browser gives String values.
