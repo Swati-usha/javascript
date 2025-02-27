4ded29958cde6589f4a0800cf7f2de2567ff7682
Swati Tyagi <57513959+Swati-usha@users.noreply.github.com>
2025-02-27 06:38:52 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail ='swati.tyagi@tcs.com'
+var accountPassword ="12345"
+accountCity ="Jaipur"
+
+// accountId = 44 // not allowed
+
+accountEmail = "ggahh"
+accountPassword="21223"
+accountCity ="Bengaluru"
+console.log(accountId);
+let accountState;
+console.table([accountId,accountEmail,accountCity,accountState])
+/*
+Prefer not to use Var
+because of issue in block scope and functional scope
+*/
