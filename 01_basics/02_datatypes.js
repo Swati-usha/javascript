f738144e36d08fe7e9d12210c9d32647429d0af4
Swati Tyagi <57513959+Swati-usha@users.noreply.github.com>
2025-02-27 06:37:59 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello") // we are using node.js, not browser
+
+console.log(3 + 3)
+console.log("Swati")
+
+// Code readability should be high
+
+let name = "Swati"
+let age = 18
+let isLoggedIn = false
+let state = null
+/*primitive data types */
+// number => 2 to power 53
+// bigint =>used to sotre bigger values of data.
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not defined
+// symbol => unique
+
+// object
+
+console.log(typeof(null))// null is of OBJECT data type
+console.log(typeof undefined) // undefined type.
